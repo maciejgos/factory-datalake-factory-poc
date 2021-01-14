9f8974ad2b1975c58a6ea5de932234fb3f2cb153
Maciej Gos <maciej@maciejgos.com>
2021-01-14 19:30:38 +0100

    Add Azure Synapse

--- src/setup.sh
+++ src/setup.sh
@@ -2,6 +2,8 @@
 RESOURCE_GROUP_NAME=rg-datawarehouse-poc
 LOCATION=westeurope
 DB_STORAGE_ACCOUNT=wwiimportersdbdata001
+SQL_USER=sqluser
+SQL_USER_PASSWORD=P@ssw0rd!
 
 echo "Create Resource Group"
 az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
@@ -12,8 +14,11 @@ az storage account create --name $DB_STORAGE_ACCOUNT --resource-group $RESOURCE_
 # Upload backup [TODO]
 
 # Deploy ARM template
+echo "Deploy ARM template"
 az deployment group create --name Initial --resource-group $RESOURCE_GROUP_NAME --template-file template.json --parameters template.parameters.json
 
 # Create Data Factory [TODO]
 
-# Create Synapse [TODO]
+# Create Synapse
+echo "Create Azure Synapse"
+az synapse workspace create --name wwi-importers-synapse --resource-group $RESOURCE_GROUP_NAME --storage-account wwiimportersdatalake001 --file-system data --sql-admin-login-user $SQL_USER --sql-admin-login-password $SQL_USER_PASSWORD --location $LOCATION
